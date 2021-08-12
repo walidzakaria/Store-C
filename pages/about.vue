05d877f7c711962288e73292f63c5556571e1e63
Walid <walidpiano@yahoo.com>
2021-08-12 02:53:36 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is about</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'About',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
